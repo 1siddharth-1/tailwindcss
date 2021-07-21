d1a35ac3dfc9246d4ccbe4a97f339b833f99624b
Siddharth Singh <singhsiddharth656@gmail.com>
2021-07-22 00:27:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily:{
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+
+  // newly added for complex classes
+  experimental:{
+    applyComplexClasses: true,
+  },
+}
